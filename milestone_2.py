8189d64f042b77ced0b9cae55f5f3918bb6365e0
jamesskinner1 <107134234+jamesskinner1@users.noreply.github.com>
2023-11-03 11:21:22 +0200

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,1 +1,21 @@
+# milestone_2.py
 
+import random
+
+# favorite fruits
+favorite_fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
+# Assign the list to the variable word_list
+word_list = favorite_fruits
+# Select a random word from the word_list
+word = random.choice(word_list)
+
+# Ask the user to enter a single letter and assign it to the variable guess
+guess = input("Enter a single letter: ")
+
+# single alphabetical character
+if len(guess) == 1 and guess.isalpha():
+    # If the check passes, print "Good guess!"
+    print("Good guess!")
+else:
+    # If the check fails, print an error message
+    print("Oops! That is not a valid input.")
